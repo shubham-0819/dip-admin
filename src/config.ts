935f944c38da48590b88289d007a01950b10fe95
praveen kumar <praveenk15499@gmail.com>
2024-11-04 21:54:23 +0530

    update

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:9091/api";
+export const API_URL = "http://ec2-18-212-106-184.compute-1.amazonaws.com:9091/api";
 export const ACCESS_TOKEN = localStorage.getItem("accessToken");
